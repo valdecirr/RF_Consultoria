e748900de3cfa8da48059f1eba3adc04ea2a6193
Valdecir <valdecirrk@gmail.com>
2023-07-23 23:25:48 -0300

    Dados atualizados

--- tabelaprincipal.php
+++ tabelaprincipal.php
@@ -118,7 +118,7 @@ include_once("config.php");
                             while ($row = mysqli_fetch_array($result)) {
                                 $resultdata = date('d/m/Y', strtotime($row['datainicio'] . ' + ' . $row['dias'] . 'days'));
                                 echo "<tr>";
-                                echo "<td><a href='Entrada.php?id={$row['id']}'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pen-fill' viewBox='0 0 16 16'>
+                                echo "<td><a href='editaritem.php?id={$row['id']}'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pen-fill' viewBox='0 0 16 16'>
                                 <path d='m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z'/>
                               </svg></a></td>";
                                 echo "<td>" . $row['nome'] . "</td>";
